f0f25c5af9ff10882ac92d9595ec65e23e3affde
MatiAlva <83080975+MatiAlva@users.noreply.github.com>
2022-11-24 12:10:22 -0300

    Se comienza con la nav

--- FrontEnd/E-commerce/src/components/Navbar/index.jsx
+++ FrontEnd/E-commerce/src/components/Navbar/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Logo from '../../assets/img/logo.png'
+
+// Icono de Lupita 
+import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai'
+
+
+//Importar imagen
+//<img src={} alt={} />
+
+
+// Icono de Lupita 
+// Se importa asi : <AiOutlineSearch />
+
+// Icono del carrito 
+// Se importa asi : <AiOutlineShoppingCart />
 
 const index = () => {
     return (
-        <div>index</div>
+        <div>
+            Navbar
+        </div>
     )
 }
 
