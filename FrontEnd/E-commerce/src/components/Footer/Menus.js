0ec40efba7113fa624ceb45d52a396632b029c32
MatiAlva <83080975+MatiAlva@users.noreply.github.com>
2022-11-28 20:33:34 -0300

    Añadir imagenes en el footer y acomodar las cards que se rompio: usear Tailwind

---
+++ FrontEnd/E-commerce/src/components/Footer/Menus.js
@@ -0,0 +1,29 @@
+export const INFORMACION = [
+    { name: "Nosotros", link: "#" },
+    { name: "Politica de Privacidad", link: "#" },
+    { name: "Politica de Garantia", link: "#" },
+    { name: "Terminos y condiciones", link: "#" },
+    { name: "Preguntas frecuentes", link: "#" },
+    { name: "Como comprar?", link: "#" },
+    { name: "Boton de arrepentimiento", link: "#" },
+    { name: "Seguimiento", link: "#" },
+];
+export const CONTACTANOS = [
+    { name: "Quiero preguntar", link: "#" },
+    { name: "WhatsApp", link: "#" },
+    { name: "Lunes a Viernes de 9:00 a 18:00 Sábados de 9:00 a 13:000", link: "#" },
+    { name: "Correo: correo@gmail.com", link: "#" },
+
+];
+export const REDES = [
+    { name: "Seguinos en nuestras redes", link: "#" },
+];
+
+
+export const Icons = [
+    { name: "logo-facebook", link: "#" },
+    { name: "logo-twitter", link: "#" },
+    { name: "logo-github", link: "#" },
+    { name: "logo-linkedin", link: "#" },
+    { name: "logo-instagram", link: "#" },
+];
